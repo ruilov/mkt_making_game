90d2e887bd90b3a923aea52e42cfc2a21cfd4f31
Rui Viana <ruilov@gmail.com>
2014-10-06 21:46:47 -0400

    removing login

--- mktmaking.py
+++ mktmaking.py
@@ -2,7 +2,6 @@ import webapp2
 from server_controllers import main,quizzes,quiz_editor,login
 
 application = webapp2.WSGIApplication([
-  ('/login/',login.MyHandler),
   ('/quizzes_api/',quizzes.Quizzes),
   (r'/quiz_editor_api/(.*)',quiz_editor.QuizEditor),
   (r'/(.*)', main.MainPage),
