6971cb9415e1ee6889193a5b2845fa78c564649e
Rui Viana <ruilov@gmail.com>
2014-10-11 22:25:41 -0400

    number formatting

--- js/quiz.js
+++ js/quiz.js
@@ -28,4 +28,57 @@ app.controller( "quizController", function userController($scope,$http,$location
     var req = $http.post("/quiz_api/?id="+quiz_id,$scope.quiz);
     req.success(quiz_api_cb);
   };
-})
+});
+
+
+app.directive('format', function ($filter) {
+  'use strict';
+
+  return {
+    require: '?ngModel',
+    link: function (scope, elem, attrs, ctrl) {
+      if (!ctrl) {
+        return;
+      }
+
+      ctrl.$formatters.unshift(function () {
+        return $filter('number')(ctrl.$modelValue);
+      });
+
+      ctrl.$parsers.unshift(function (viewValue) {
+        // get the stuff after the decimal place as filter(number) insists in round to 3 and if I pass fractionSize as an argument
+        // then it formats to that size...
+        var postfix = "";
+        var hasPost = false;
+        var idx = viewValue.indexOf(".");
+        if(idx!=-1) {
+          hasPost = true;
+          postfix = viewValue.substr(idx+1);
+          var idx2 = postfix.indexOf(".");
+          if(idx2!=-1) {
+            elem.val("");
+            return 0;
+          }
+
+          if(postfix.length>0) {
+            postfix = $filter('number')(postfix);
+            if(postfix.length==0) {
+              elem.val("");
+              return 0;
+            }
+          }
+          postfix = postfix.replace(/[\,]/g, '');
+          viewValue = viewValue.substr(0,idx);
+        }
+
+        var plainNumber = viewValue.replace(/[\,]/g, '');
+        var elemVal = $filter('number')(plainNumber);
+        if(hasPost) elemVal += "." + postfix;
+        elem.val(elemVal);
+
+        // console.log(viewValue + " , " + plainNumber + " , " + b);
+        return plainNumber;
+      });
+    }
+  };
+});
