efbc9ff67da1d566b1027898bcf38000eb19af40
Rui Viana <ruilov@gmail.com>
2015-11-15 18:17:37 -0500

    catching up, there's no more id

--- python/signout.py
+++ python/signout.py
@@ -2,6 +2,6 @@ import webapp2
 
 class SignOut(webapp2.RequestHandler):
   def get(self):
-    self.response.delete_cookie('id')
+    self.response.delete_cookie('username')
     self.response.delete_cookie('hash')
     self.redirect('/')
